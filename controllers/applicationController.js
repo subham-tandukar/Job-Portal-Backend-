940849456f8a50856838653e5506fe6bfaec49e9
Subham <subhamtndkr9@gmail.com>
2024-05-04 00:20:15 +0545

    commit

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -48,7 +48,7 @@ exports.applyJob = async (req, res) => {
     const cvUrl = cloudinaryUploadResponse.secure_url;
 
     // Save the PDF file to the server
-    const filePath = path.join(__dirname, "../uploads", filename);
+    const filePath = path.join("/tmp", filename);
     fs.writeFileSync(filePath, pdfBuffer);
     // Return the URL for accessing the uploaded PDF
     const fileUrl = `${process.env.REACT_APP_URL}/uploads/${filename}`;
