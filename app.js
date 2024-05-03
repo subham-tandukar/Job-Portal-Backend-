d5d9fbcccef0c545d1b88bc6580c299ef8193807
Subham <subhamtndkr9@gmail.com>
2024-05-04 02:03:51 +0545

    comit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(router);
 
 // Serve static files from the 'uploads' directory
 
-app.use("/tmp", express.static(path.join(__dirname, "uploads")));
+app.use("/tmp", express.static(path.join(__dirname, "tmp")));
 
 app.get("/", (req, res) => {
   res.json("server start");
